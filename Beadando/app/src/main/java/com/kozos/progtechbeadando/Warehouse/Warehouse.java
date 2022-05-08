1bd56f511b58dbc7c344da788f683b505759d92e
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-05-08 14:37:21 +0200

    Create Warehouse interface

---
+++ Beadando/app/src/main/java/com/kozos/progtechbeadando/Warehouse/Warehouse.java
@@ -0,0 +1,22 @@
+package com.kozos.progtechbeadando.Warehouse;
+
+import com.kozos.progtechbeadando.Customers.Customer;
+import com.kozos.progtechbeadando.Order.Order;
+import com.kozos.progtechbeadando.Products.Product;
+
+import java.util.List;
+
+public interface Warehouse {
+
+    void addProduct(Product product);
+    Product getProduct(String id);
+    List<Product> getProducts();
+    void addNewOrder(Order order);
+    Order getOrder(String id);
+    List<Order> getOrders();
+    void addNewCustomer(Customer customer);
+    Customer getCustomer(String id);
+    List<Customer> getCustomers();
+    void applyWarranty(String orderId, String productId);
+
+}
