0c6c747fdb0b3d37813298bdf52ef081d8c31550
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-05-08 16:53:58 +0200

    Update MainActivity class
    For Databases

--- Beadando/app/src/main/java/com/kozos/progtechbeadando/AndroidEssentials/MainActivity.java
+++ Beadando/app/src/main/java/com/kozos/progtechbeadando/AndroidEssentials/MainActivity.java
@@ -1,9 +1,15 @@
 package com.kozos.progtechbeadando.AndroidEssentials;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.kozos.progtechbeadando.Database.ChangeLogOpenHelper;
+import com.kozos.progtechbeadando.Database.CustomersOpenHelper;
+import com.kozos.progtechbeadando.Database.OrdersOpenHelper;
+import com.kozos.progtechbeadando.Database.ProductsContract;
+import com.kozos.progtechbeadando.Database.ProductsOpenHelper;
 import com.kozos.progtechbeadando.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProductsOpenHelper productsOpenHelper = new ProductsOpenHelper(this);
+        CustomersOpenHelper customersOpenHelper = new CustomersOpenHelper(this);
+        OrdersOpenHelper ordersOpenHelper = new OrdersOpenHelper(this);
+        ChangeLogOpenHelper changeLogOpenHelper = new ChangeLogOpenHelper(this);
+
+        SQLiteDatabase products = productsOpenHelper.getWritableDatabase();
+        SQLiteDatabase customers = customersOpenHelper.getWritableDatabase();
+        SQLiteDatabase orders = ordersOpenHelper.getWritableDatabase();
+        SQLiteDatabase changeLogs = changeLogOpenHelper.getWritableDatabase();
     }
 }
